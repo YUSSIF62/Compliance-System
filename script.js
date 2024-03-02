95f49cbd46e5e296d66b9a674947fda819a70337
YUSSIF ABDUL-RAHAMAN <abdulrahamanyussif62@gmail.com>
2024-03-03 00:41:25 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function openComplianceStatus() {
+    // Simulate opening a new page for compliance status
+    alert("This will open your compliance status page (functionality not implemented here)");
+  }
+  
